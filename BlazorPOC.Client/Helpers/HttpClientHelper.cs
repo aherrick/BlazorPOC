117897358f07d6d9ac8b1552c006bd90a7b6dfa4
Andrew Herrick <aj@andrewherrick.com>
2020-02-14 09:43:46 -0500

    Initial commit

---
+++ BlazorPOC.Client/Helpers/HttpClientHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace BlazorPOC.Client.Helpers
+{
+    public static class HttpClientHelper
+    {
+        public static HttpClient Client { get; } = new HttpClient()
+        {
+            BaseAddress = new Uri("https://localhost:44357/")
+        };
+    }
+}
---
+++ BlazorPOC.Client/ViewModels/FetchDataViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BlazorPOC.Client.Helpers;
+using BlazorPOC.Server;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorPOC.Client.ViewModels
+{
+    public class FetchDataViewModel
+    {
+        public List<WeatherForecast> WeatherForecasts { get; set; }
+
+        public FetchDataViewModel()
+        {
+        }
+
+        public async Task GetData()
+        {
+            var json = await HttpClientHelper.Client.GetStringAsync("api/weatherforecast");
+
+            WeatherForecasts = JsonConvert.DeserializeObject<List<WeatherForecast>>(json);
+        }
+    }
+}
